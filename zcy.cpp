6ed0786d8befa4c9e42949dcbb059d65f4f15488
yan666-m <142365097+yan666-m@users.noreply.github.com>
2023-08-18 09:47:21 +0800

    Update zcy.cpp

--- zcy.cpp
+++ zcy.cpp
@@ -17,10 +17,31 @@ public:
 		s=b*b;
 		cout<<"正方形面积为"<<s<<"平方厘米"<<endl;
 	};
+void yuanxing()     //圆形面积
+        {
+                double s, r, S;
+                cout << "请输入圆形的半径:" << endl;
+                cin >> r;
+                s = 3.1416 * r * r;
+                int x;
+                cout << "请选择单位" << endl << "1.厘米，2.英寸";
+                cin >> x;
+                if (x == 1)S = s;
+                else S = 6.4516 * s;
+                cout << "圆形面积为" << S << "平方厘米";
+        };
 
 }；
 int main()
 {
 	mianji ss;
-	ss.zhengfangxing();
+	  cout<<"请选择图形-1、圆形；2、正方形"<<endl;
+        int choice;
+        cin>>choice;
+        switch(choice)
+        {
+                case(1):ss.yuanxing();
+                case(2):ss.zhengfangxing();
+        }
+
 }
